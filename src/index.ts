a551c582c033c35eaf1a5d36f25d5b80b6c9878a
jayshankar-dey <deyjayshankar@gmail.com>
2025-03-13 12:54:05 +0530

    create src

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+export class EmailSend {
+       private transporter;
+     
+       constructor() {
+         this.transporter = nodemailer.createTransport({
+           service: "gmail",
+           auth: {
+             user: process.env.EMAIL, // Your email
+             pass: process.env.PASSWORD, // App password or SMTP password
+           },
+         });
+       }
+     
+       async sendEmail(from:string,to: string, subject: string, text: string, html?: string): Promise<void> {
+         try {
+           const mailOptions = {
+             from,
+             to,
+             subject,
+             text,
+             html,
+           };
+     
+           const info = await this.transporter.sendMail(mailOptions);
+           console.log("📧 Email sent: " + info.response);
+         } catch (error) {
+           console.error("❌ Error sending email:", error);
+           throw error;
+         }
+       }
+     }
